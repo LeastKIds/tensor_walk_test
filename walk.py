7ea1b3f62e82d580829bef518f4027acf8c9baa1
leastkids <leastkids@gmail.com>
2021-10-29 15:42:06 +0900

    map data save to .cvs

--- walk.py
+++ walk.py
@@ -47,99 +47,101 @@ for uu, vv, kkey, ddata in G.edges(keys=True, data=True):
 
 # print(data[0])
 # print(data[1])
-# print(data[2])
+# print(data[10]['name'])
+
+time_pd = pd.DataFrame(data)
+time_pd.to_csv('data.csv',mode='w',encoding='utf-8-sig')
 
 
-
-###############################################################################
-#                5. Assign Each Segment a Color Based on its Length           #
-###############################################################################
-# List to store colors
-
-roadColors = []
-
-# The length is in meters
-for item in data:
-    if "length" in item.keys():
-
-        if item["length"] <= 100:
-            color = "#d40a47"
-
-        elif item["length"] > 100 and item["length"] <= 200:
-            color = "#e78119"
-
-        elif item["length"] > 200 and item["length"] <= 400:
-            color = "#30bab0"
-
-        elif item["length"] > 400 and item["length"] <= 800:
-            color = "#bbbbbb"
-
-        else:
-            color = "w"
-
-    roadColors.append(color)
-
-###############################################################################
-#                6. Assign Each Segment a Width Based on its type             #
-###############################################################################
-# List to store linewidths
-
-roadWidths = []
-
-for item in data:
-    if "footway" in item["highway"]:
-        linewidth = 1
-
-    else:
-        linewidth = 2.5
-
-    roadWidths.append(linewidth)
-
-###############################################################################
-#                                 7. Make Map                                 #
-###############################################################################
-# Center of map
-latitude = 35.923364943153565
-longitude = 128.5764782484241
-
-# Bbox sides
-north = latitude + 0.05
-south = latitude - 0.05
-east = longitude + 0.08
-west = longitude - 0.08
-
-# Make Map
-fig, ax = ox.plot_graph(G, node_size=0, bbox=(north, south, east, west),  dpi=600, bgcolor="#061529",
-                        save=False, edge_color=roadColors,
-                        edge_linewidth=roadWidths, edge_alpha=1)
-
-# Text and marker size
-markersize = 5
-fontsize = 5
-
-# Add legend
-legend_elements = [Line2D([0], [0], marker='s', color="#061529", label='Length < 100 m',
-                          markerfacecolor="#d40a47", markersize=markersize),
-
-                   Line2D([0], [0], marker='s', color="#061529", label='Length between 100-200 m',
-                          markerfacecolor="#e78119", markersize=markersize),
-
-                   Line2D([0], [0], marker='s', color="#061529", label='Length between 200-400 m',
-                          markerfacecolor="#30bab0", markersize=markersize),
-
-                   Line2D([0], [0], marker='s', color="#061529", label='Length between 400-800 m',
-                          markerfacecolor="#bbbbbb", markersize=markersize),
-
-                   Line2D([0], [0], marker='s', color="#061529", label='Length > 800 m',
-                          markerfacecolor="w", markersize=markersize)]
-
-l = ax.legend(handles=legend_elements, bbox_to_anchor=(0.0, 0.0), frameon=True, ncol=1,
-              facecolor='#061529', framealpha=0.9,
-              loc='lower left', fontsize=fontsize, prop={'family': "Georgia", 'size': fontsize})
-
-# Legend font color
-for text in l.get_texts():
-    text.set_color("w")
-
-# Save figure
-fig.savefig("Lawrence.png", dpi=1000, bbox_inches='tight', format="png", facecolor=fig.get_facecolor(), transparent=True)
+# ###############################################################################
+# #                5. Assign Each Segment a Color Based on its Length           #
+# ###############################################################################
+# # List to store colors
+#
+# roadColors = []
+#
+# # The length is in meters
+# for item in data:
+#     if "length" in item.keys():
+#
+#         if item["length"] <= 100:
+#             color = "#d40a47"
+#
+#         elif item["length"] > 100 and item["length"] <= 200:
+#             color = "#e78119"
+#
+#         elif item["length"] > 200 and item["length"] <= 400:
+#             color = "#30bab0"
+#
+#         elif item["length"] > 400 and item["length"] <= 800:
+#             color = "#bbbbbb"
+#
+#         else:
+#             color = "w"
+#
+#     roadColors.append(color)
+#
+# ###############################################################################
+# #                6. Assign Each Segment a Width Based on its type             #
+# ###############################################################################
+# # List to store linewidths
+#
+# roadWidths = []
+#
+# for item in data:
+#     if "footway" in item["highway"]:
+#         linewidth = 1
+#
+#     else:
+#         linewidth = 2.5
+#
+#     roadWidths.append(linewidth)
+#
+# ###############################################################################
+# #                                 7. Make Map                                 #
+# ###############################################################################
+# # Center of map
+# latitude = 35.923364943153565
+# longitude = 128.5764782484241
+#
+# # Bbox sides
+# north = latitude + 0.05
+# south = latitude - 0.05
+# east = longitude + 0.08
+# west = longitude - 0.08
+#
+# # Make Map
+# fig, ax = ox.plot_graph(G, node_size=0, bbox=(north, south, east, west),  dpi=600, bgcolor="#061529",
+#                         save=False, edge_color=roadColors,
+#                         edge_linewidth=roadWidths, edge_alpha=1)
+#
+# # Text and marker size
+# markersize = 5
+# fontsize = 5
+#
+# # Add legend
+# legend_elements = [Line2D([0], [0], marker='s', color="#061529", label='Length < 100 m',
+#                           markerfacecolor="#d40a47", markersize=markersize),
+#
+#                    Line2D([0], [0], marker='s', color="#061529", label='Length between 100-200 m',
+#                           markerfacecolor="#e78119", markersize=markersize),
+#
+#                    Line2D([0], [0], marker='s', color="#061529", label='Length between 200-400 m',
+#                           markerfacecolor="#30bab0", markersize=markersize),
+#
+#                    Line2D([0], [0], marker='s', color="#061529", label='Length between 400-800 m',
+#                           markerfacecolor="#bbbbbb", markersize=markersize),
+#
+#                    Line2D([0], [0], marker='s', color="#061529", label='Length > 800 m',
+#                           markerfacecolor="w", markersize=markersize)]
+#
+# l = ax.legend(handles=legend_elements, bbox_to_anchor=(0.0, 0.0), frameon=True, ncol=1,
+#               facecolor='#061529', framealpha=0.9,
+#               loc='lower left', fontsize=fontsize, prop={'family': "Georgia", 'size': fontsize})
+#
+# # Legend font color
+# for text in l.get_texts():
+#     text.set_color("w")
+#
+# # Save figure
+# fig.savefig("Lawrence.png", dpi=1000, bbox_inches='tight', format="png", facecolor=fig.get_facecolor(), transparent=True)
