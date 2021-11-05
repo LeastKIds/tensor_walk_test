8ec5413749a6bcf8c9a645db41306c9f107a1423
leastkids <leastkids@gmail.com>
2021-11-05 13:46:33 +0900

    runtime check 2

--- algorithm2.py
+++ algorithm2.py
@@ -20,7 +20,7 @@ def route(list, g, start, end):
 
     route = nx.shortest_path(g, list[-1], end_node, weight='length')
     route_sum.extend(route[1:])
-    fig, ax = ox.plot_graph_route(g, route_sum, node_size=0)
+    # fig, ax = ox.plot_graph_route(g, route_sum, node_size=0)
     print(length_sum)
     print(time_sum)
 
@@ -30,20 +30,30 @@ G = ox.graph_from_point(location_point, dist=2000, dist_type='bbox', network_typ
 
 start_time = time.time()
 
-list1 = ox.nearest_nodes(G, 128.63568,35.90232)
-list2 = ox.nearest_nodes(G, 128.60259,35.92024)
-list3 = ox.nearest_nodes(G, 128.59561,35.90047)
-list4 = ox.nearest_nodes(G, 128.59561,35.90047)
-list5 = ox.nearest_nodes(G, 128.60896,35.90016)
-list6 = ox.nearest_nodes(G, 128.61080,35.89523)
-list7 = ox.nearest_nodes(G, 128.61500,35.89988)
-list8 = ox.nearest_nodes(G, 128.61432,35.89620)
-list9 = ox.nearest_nodes(G, 128.61449,35.89398)
-list10 = ox.nearest_nodes(G, 128.61234,35.88536)
+# list1 = ox.nearest_nodes(G, 128.63568,35.90232)
+# list2 = ox.nearest_nodes(G, 128.60259,35.92024)
+# list3 = ox.nearest_nodes(G, 128.59561,35.90047)
+# list4 = ox.nearest_nodes(G, 128.59561,35.90047)
+# list5 = ox.nearest_nodes(G, 128.60896,35.90016)
+# list6 = ox.nearest_nodes(G, 128.61080,35.89523)
+# list7 = ox.nearest_nodes(G, 128.61500,35.89988)
+# list8 = ox.nearest_nodes(G, 128.61432,35.89620)
+# list9 = ox.nearest_nodes(G, 128.61449,35.89398)
+# list10 = ox.nearest_nodes(G, 128.61234,35.88536)
+list1 = list(G)[0]
+list2 = list(G)[10]
+list3 = list(G)[20]
+list4 = list(G)[30]
+list5 = list(G)[40]
+list6 = list(G)[50]
+list7 = list(G)[60]
+list8 = list(G)[70]
+list9 = list(G)[80]
+list10 = list(G)[90]
 
 ss_node = (128.62200, 35.89629)
 ee_node = (128.60530, 35.89601)
-route_list = [list1]
+route_list = [list1,list2, list3,list4, list5, list6, list7, list8, list9, list10]
 
 
 route(route_list, G, ss_node, ee_node)
@@ -51,7 +61,9 @@ print(time.time() - start_time)
 
 # 왔던길 다시 되돌아가는 경우 제거
 # route_sum 안에 있는 중복된 노드를 제거(점을 기준으로 하기에 왔던길이 아니라 왔던 골목이 제거됨)
-# 1개의 추가된 점 : 4초
-# 2개의 추가된 점 : 4초
-# 3개의 추가된 점 : 4초
-# 10개의 추가된 점 : 6.8초
+
+# 지도에 표시하지 않고　시간만 체크했을 시
+# 1개의 추가된 점 : 0.08초, 0.072초, 0.091초
+# 2개의 추가된 점 : 0.13초, 0.13초, 0.13초
+# 3개의 추가된 점 : 0.14초, 0.14초, 0.14초
+# 10개의 추가된 점 : 0.3초, 0,31초, 033초
